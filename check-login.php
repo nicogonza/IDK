03c0691470f2305b6c26762e7c6252811efe69da
Kavy Rattana <akaoctech@gmail.com>
2014-10-25 22:02:33 -0400

    test

--- check-login.php
+++ check-login.php
@@ -1,10 +1,7 @@
-
 <?php
 
 ob_start();
 require('mysqli_connect.php');
-mysql_connect(localhost,gonzodev_kavy,hacknc2014 )or die("cannot connect"); 
-mysql_select_db("gonzodev_idk")or die("cannot select DB");
 
 // get $myusername and $mypassword from login.php
 $myusername=$_POST['myusername']; 
@@ -13,13 +10,13 @@ $mypassword=$_POST['mypassword'];
 
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
-$myusername = mysql_real_escape_string($myusername);
-$mypassword = mysql_real_escape_string($mypassword);
+$myusername = mysqli_real_escape_string($myusername);
+$mypassword = mysqli_real_escape_string($mypassword);
 $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-$result=mysql_query($sql);
+$results = @mysqli_query ($dbc, $sql);
 
 // is there such user
-$count=mysql_num_rows($result);
+$count=mysqli_num_rows($results);
 
 if($count==1){
 
