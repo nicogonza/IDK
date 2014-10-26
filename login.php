1f78056a6f7b108b06b41b650e7af155426a9d94
gonzodevs <n_gonzal@uncg.edu>
2014-10-25 20:32:26 -0400

    ..

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 
 <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
 <tr>
-<form name="form1" method="post" action="check-login.php">
+<form name="form1" method="post" action="checklogin.php">
 <td>
 <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
 <tr>
