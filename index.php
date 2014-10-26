b6ce808422fb84d6263bbdd4ed8215523dc6b45f
Kavy Rattana <akaoctech@gmail.com>
2014-10-26 03:35:17 -0400

    index page

--- index.php
+++ index.php
@@ -3,20 +3,22 @@ session_start();
 ?>
 <html>
 <head>
-	<title> IDK Application </title>
-        <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<title>IDK Application</title>
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
-    <div class="prof-login">
-<form name="form1" method="post" action="check-login.php">
-        
-    <h2 class="form-signin-heading">Professor log in</h2>
-        <input name="myusername" type="myusername" placeholder="Email address" required autofocus id="myusername"><br>
-        <input name="mypassword" type="password" id="mypassword" placeholder="Password" required><br>
-        <input name="login" value="Log in" type="submit"></input>
-      </form>
-</div> <!-- prof-login-->
-    </div> <!-- /container -->
+  <div class="prof-login">
+    <form name="form1" method="post" action="check-login.php">
+      <h2 class="form-signin-heading">Log In</h2>
+      <input name="prof" value="Log in" type="submit">
+      </input>
+      <input name="student" value="Log in" type="submit">
+      </input>
+    </form>
+  </div>
+  <!-- prof-login--> 
+</div>
+<!-- /container -->
 </body>
