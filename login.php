ef865ece064b38eb4a7a68aeaf84babf2c58b537
gonzodevs <n_gonzal@uncg.edu>
2014-10-26 02:24:12 -0400

    login fiz

--- login.php
+++ login.php
@@ -24,8 +24,8 @@
 <form name="form1" method="post" action="check-login.php">
         
     <h2 class="form-signin-heading">Professor log in</h2>
-        <input type="myusername" placeholder="Email address" required autofocus id="myusername"><br>
-        <input type="password" id="mypassword" placeholder="Password" required><br>
+        <input name="myusername" type="myusername" placeholder="Email address" required autofocus id="myusername"><br>
+        <input name="mypassword" type="password" id="mypassword" placeholder="Password" required><br>
         <input name="login" value="Log in" type="submit"></input>
       </form>
 </div> <!-- prof-login-->
