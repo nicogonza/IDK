84ae562e1fb6847e26c48c2279a5188ef6e4354f
m152196 <m152196@usna.edu>
2014-10-25 23:55:32 -0400

    slogin check

---
+++ check-slogin.php
@@ -0,0 +1,57 @@
+<?php
+if (isset($_POST['login'])) {
+
+	// For processing the login:<p></p>
+	require ('login_functions.php');
+	
+	// Need the database connection:
+	require ('mysqli_connect.php');
+		
+	// Check the login:
+	list ($check, $data) = check_login($dbc, $_POST['myusername'], $_POST['mypassword']);
+	if ($check) { // OK!
+	
+                $_SESSION['username']=$data['username'];
+				
+				$q = "select * from professors where username= \"".$_SESSION['username']."\";";	
+		
+				$r = @mysqli_query ($dbc, $q); 
+				
+				if($r) {
+					
+				$row = mysqli_fetch_array($r);
+				
+				} else {
+					
+					echo '<h1>System Error</h1>
+			<p class="error">There are no entries in the table</p>'; 
+	
+			echo '<p>' . mysqli_error($dbc) . '<br><br>Query: ' . $q . '</p>';
+				}
+		
+		// Redirect:
+		redirect_user('studentinput.php');
+			
+	} else { // Unsuccessful!
+
+		// Assign $data to $errors for error reporting
+		// in the login_page.php file.
+		$errors = $data;
+
+	}
+	  	
+	mysqli_close($dbc); // Close the database connection.
+
+} // End of the main submit conditional.
+// Print any error messages, if they exist:
+if (isset($errors) && !empty($errors)) {
+	echo '<h1>Error!</h1>
+	<p class="error">The following error(s) occurred:<br>';
+	foreach ($errors as $msg) {
+		echo " - $msg<br>\n";
+	}
+	echo '</p><p>Please try again.</p>';
+}
+
+include('slogin.php');
+?>
--- slogin.php
+++ slogin.php
@@ -5,7 +5,7 @@
 <div class="student-login">
 	<h1 id="header"> Student Login</h1>
 
-	<form name="form1" method="post" action="check-login.php">
+	<form name="form1" method="post" action="check-slogin.php">
 	<input name="courseid" type="text" id="courseid" placeholder="Course ID">
 	<input type="submit" name="login" value="Login">
 	</form>
