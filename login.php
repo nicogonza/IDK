289ddd494582c8ac5bccd1e73cb5810c7c3b72af
m152196 <m152196@usna.edu>
2014-10-25 22:11:50 -0400

    removed table layout

--- login.php
+++ login.php
@@ -5,26 +5,10 @@
 <div class="login">
 	<h1 id="header"> IDK, Professor login page</h1>
 
-	<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-	<tr>
 	<form name="form1" method="post" action="check-login.php">
-	<td>
-	<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-	<tr>
-	<td width="294"><input name="myusername" type="text" id="myusername" placeholder="Username"></td>
-	</tr>
-	<tr>
-	<td><input name="mypassword" type="password" id="mypassword" placeholder="Password"></td>
-	</tr>
-	<tr>
-	<td>&nbsp;</td>
-	<td>&nbsp;</td>
-	<td><input type="submit" name="login" value="Login"></td>
-	</tr>
-	</table>
-	</td>
+	<input name="myusername" type="text" id="myusername" placeholder="Username">
+	<input name="mypassword" type="password" id="mypassword" placeholder="Password">
+	<input type="submit" name="login" value="Login">
 	</form>
-	</tr>
-	</table>
 </div>
 </HTML>
