1b7308db8b815e0d40af89956d4966b5590b6fb4
Kavy Rattana <akaoctech@gmail.com>
2014-10-25 23:29:54 -0400

    edit

--- login_page.php
+++ login_page.php
@@ -2,19 +2,6 @@
 // This page prints any errors associated with logging in
 // and it creates the entire login page, including the form.
 
-// Include the header:
-$page_title = 'Login';
-
-// Print any error messages, if they exist:
-if (isset($errors) && !empty($errors)) {
-	echo '<h1>Error!</h1>
-	<p class="error">The following error(s) occurred:<br>';
-	foreach ($errors as $msg) {
-		echo " - $msg<br>\n";
-	}
-	echo '</p><p>Please try again.</p>';
-}
-
 // Display the form:
 ?><h1>Login</h1>
 <form action="index.php?page=login" method="post">
