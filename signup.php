921ef91929dda73317162907df30c90b68120a9d
m152196 <m152196@usna.edu>
2014-10-25 21:54:30 -0400

    update

--- signup.php
+++ signup.php
@@ -7,19 +7,19 @@
 	<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-<!-- login div -->
-<div class="login">
-<!-- login header -->
-  <h1 id="header">Sign up in seconds!</h1>
-<!-- start the signup process -->
-  <form id="form" action="signup.php">
-    <input type="text" placeholder="What's your username?" maxlength="16"><br />
-    <input type="password" placeholder="Choose a password"><br />
-<!--	<input type="radio" name="type" value="teacher">Teacher<br />
-	<input type="radio" name="type" value="student">Student<br /> -->
-    <input type="button" value="Student Sign Up"><br /><br />
-	<input type="button" value="Teacher Sign Up">
-  </forn>
-</login>
+	<!-- login div -->
+	<div class="login">
+	<!-- login header -->
+		<h1 id="header">Sign up in seconds!</h1>
+		<!-- start the signup process -->
+		<form id="form" action="signup.php">
+		<input type="text" placeholder="What's your username?" maxlength="16"><br />
+		<input type="password" placeholder="Choose a password"><br />
+		<!--	<input type="radio" name="type" value="teacher">Teacher<br />
+		<input type="radio" name="type" value="student">Student<br /> -->
+		<input type="button" value="Student Sign Up"><br /><br />
+		<input type="button" value="Teacher Sign Up">
+		</form>
+	</div>
 </body>
 </html>
