bc8c76df68e548fc0100cf2826dcf6a1978bbfcd
gonzodevs <n_gonzal@uncg.edu>
2014-10-26 02:07:54 -0400

    css update to login

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 if(!session_is_registered(myusername)){
+    echo 'if is true';
 header("location:login.php");
 }
 ?>
--- login.php
+++ login.php
@@ -1,14 +1,38 @@
-<HTML> 
-<head>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<div class="prof-login">
-	<h1 id="header"> Professor Login</h1>
 
-	<form name="form1" method="post" action="check-login.php">
-	<input name="myusername" type="text" id="myusername" placeholder="Username">
-	<input name="mypassword" type="password" id="mypassword" placeholder="Password">
-	<input type="submit" name="login" value="Login">
-	</form>
-</div>
-</HTML>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+      <link rel="stylesheet" href="css/style.css" />
+
+    <title>Login professor</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+<div class="container">
+    <div class="prof-login">
+<form name="form1" method="post" action="check-login.php">
+        
+    <h2 class="form-signin-heading">Professor log in</h2>
+        <input type="myusername" placeholder="Email address" required autofocus id="myusername"><br>
+        <input type="password" id="mypassword" placeholder="Password" required><br>
+        <input name="login" value="Log in" type="submit"></input>
+      </form>
+</div> <!-- prof-login-->
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
